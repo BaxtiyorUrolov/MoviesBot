b059083ea0599ce0bebd08c8cb5edd295a78c6da
Baxtiyor Urolov <uralov2908@gmail.com>
2025-04-02 21:25:40 +0500

    update token

--- cmd/main.go
+++ cmd/main.go
@@ -22,9 +22,8 @@ func main() {
 	}
 	defer db.Close()
 
-	//botToken := "7022304427:AAHlYfBrdysAYZNzrFAyZ1laDzCD7I-N_Hk"
+	botToken := "7022304427:AAHlYfBrdysAYZNzrFAyZ1laDzCD7I-N_Hk"
 
-	botToken := "6902655696:AAHnk8V-ly7Q3W57QFNvOlatOTAZrgE4juA"
 	botInstance, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Fatal(err)
